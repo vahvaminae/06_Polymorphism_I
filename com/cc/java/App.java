3cf797539a03b56e29989762adf764bdf7ca0f78
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-08-01 15:26:12 +0200

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,19 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+     
+        
+    }
+
+   
+   
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+
+
+
+}
+
